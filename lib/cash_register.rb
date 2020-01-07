f31db31a32a6d2ee2b76c7f49082eecbe64f7c9d
James Lenahan <james.k.lenahan@gmail.com>
2020-01-07 17:25:11 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -5,11 +5,18 @@ class CashRegister
     @discount = discount
     @items = []
   end
-  def add_item
+  def add_item(title, price, quantity = 1)
     if quantity > 1
       i = 0
       while i < quantity
       @items << title
       i+=1
     end
+    else
+      @items << title
+    end
+    @total += price*quantity
+    @last_transaction_amount = @total
+    @total
+  end
 end
