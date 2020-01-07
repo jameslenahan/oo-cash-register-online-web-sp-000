775748b70e5356a520fe7d7cddcb3238572afc47
James Lenahan <james.k.lenahan@gmail.com>
2020-01-07 17:54:39 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,11 +1,13 @@
 class CashRegister
-  attr_accessor :total, :discount, :last_transaction_amount, :items
+  attr_accessor :total, :discount, :price, :items
   def initialize(discount = 0)
     @total = 0
     @discount = discount
     @items = []
   end
   def add_item(title, price, quantity = 1)
+    @price = price
+    @total += price * quantity
     if quantity > 1
       i = 0
       while i < quantity
